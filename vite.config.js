f0d60f895d17fea4ab62435cab89d3b163173bcf
Henrique Herrmann <h.herrmann@estudantes.ifg.edu.br>
2026-03-10 17:29:28 -0300

    fix: ajuste rolldown vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
+    'import.meta.env.VITE_GOOGLE_CLIENT_ID': JSON.stringify(process.env.VITE_GOOGLE_CLIENT_ID),
+  },
 })
